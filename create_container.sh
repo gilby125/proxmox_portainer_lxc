0acf4285bee72a83ad04a75ebbbc739faaa63f46
Gilby <gilby125@users.noreply.github.com>
2024-05-24 20:39:43 -0500

    Update create_container.sh

--- create_container.sh
+++ create_container.sh
@@ -65,11 +65,11 @@ TEMP_DIR=$(mktemp -d)
 pushd $TEMP_DIR >/dev/null
 
 # Download setup script
-wget -qL https://github.com/whiskerz007/proxmox_portainer_lxc/raw/master/setup.sh
+wget -qL https://github.com/gilby125/proxmox_portainer_lxc/raw/master/setup.sh
 
 # Detect modules and automatically load at boot
 load_module aufs
-load_module overlay
+#load_module overlay
 
 # Select storage location
 while read -r line; do
